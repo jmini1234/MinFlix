48b5bef1154f57504a67bb6a0cd1cc912163d63f
심정민 <shjj09@naver.com>
2022-08-25 23:44:52 +0900

    ADD Carousel & Slider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,31 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/model/model_movie.dart';
+import 'package:myapp/widget/box_slider.dart';
+import 'package:myapp/widget/carousel_slider.dart';
+import 'package:myapp/widget/circle_slider.dart';
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
+class _HomeScreenState extends State<HomeScreen> {
   List<Movie> movies = [
-    Movie.fromMap(
-      {
-        'title' : '사랑의 불시착',
-        'keyword' : '사랑/로맨스/판타지',
-        'poster' : 'test_movie_1.png',
-        'like' : false
-      }
-    )
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+        Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+        Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
   ];
   @override
-  void initState(){
+  void initState() {
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context){
-    return TopBar();
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            CarousleImage(movies: movies), //가장 밑
+            TopBar(), //그 위
+          ],
+        ),
+        CirclrSlider(
+          movies : movies
+        ),
+        BoxSlider(movies: movies)
+      ],
+    );
   }
 }
+
 // homeScreen에서만 사용될 예정이므로 따로 빼지 않는다.
 class TopBar extends StatelessWidget {
   @override
@@ -35,36 +68,33 @@ class TopBar extends StatelessWidget {
       child: Row(
         // 간격
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget> [
-        Image.asset(
-          'images/bbongflix_logo.png',
-          fit: BoxFit.contain,
-          height: 25,
-        ),
-        Container(
-          padding: EdgeInsets.only(right : 1),
-          child : Text(
-            'TV 프로그램',
-            style: TextStyle(fontSize: 14),
+        children: <Widget>[
+          Image.asset(
+            'images/bbongflix_logo.png',
+            fit: BoxFit.contain,
+            height: 25,
           ),
-        ),
-        Container(
-          padding: EdgeInsets.only(right : 1),
-          child : Text(
-            '영화',
-            style: TextStyle(fontSize: 14),
-          )
-        ),
-        Container(
-          padding: EdgeInsets.only(right : 1),
-          child : Text(
-            '내가 찜한 콘텐츠',
-            style: TextStyle(fontSize: 14),
-          )
-        ),
-       ],
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.only(right: 1),
+              child: Text(
+                '영화',
+                style: TextStyle(fontSize: 14),
+              )),
+          Container(
+              padding: EdgeInsets.only(right: 1),
+              child: Text(
+                '내가 찜한 콘텐츠',
+                style: TextStyle(fontSize: 14),
+              )),
+        ],
       ),
     );
   }
 }
-
