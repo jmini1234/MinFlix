2f0038174c61f7e9736dfbc46752e61eaf86e338
심정민 <shjj09@naver.com>
2022-09-02 23:52:22 +0900

    ADD Firebase Flugin

--- lib/model/model_movie.dart
+++ lib/model/model_movie.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Movie{
   final String title;
   final String keyword;
   final String poster;
   final bool like;
+  // 해당 데이터의 CRUD를 간단하게 처리 할 수 있음
+  //final DocumentReference reference;
 
   Movie.fromMap(Map<String, dynamic> map)
   :title = map['title'],
